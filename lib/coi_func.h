a86a4fb70a4fbf8ad2a9204e10d6f7f3b1d62b69
YULANG <langyu@YULANGs-iMac.local>
2016-12-10 20:30:54 -0600

    Initial commit. README to be added later

---
+++ lib/coi_func.h
@@ -0,0 +1,17 @@
+/*
+ * taskq.h
+ *
+ *  Created on: 2015Äê4ÔÂ2ÈÕ
+ *      Author: langyu
+ */
+
+#ifndef SRC_LIB_TASKQ_H_
+#define SRC_LIB_TASKQ_H_
+
+#include "tbb/concurrent_queue.h"
+#include <pthread.h>
+#include "vina_main.h"
+#include "job_pool.h"
+#include <string.h>
+
+#endif
